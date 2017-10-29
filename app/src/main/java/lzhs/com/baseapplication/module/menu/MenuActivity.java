b1fd711d48b134188c53e95717dcab7a64c4c383
LZHS <1050629507@qq.com>
2017-10-29 15:35:23 +0800

    LZHS  首页底部布局03

--- app/src/main/java/lzhs/com/baseapplication/module/menu/MenuActivity.java
+++ app/src/main/java/lzhs/com/baseapplication/module/menu/MenuActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -30,11 +33,31 @@ public class MenuActivity extends BaseActivity {
 
     @BindView(R.id.mVPFmain)
     ViewPagerFixed mVPFmain;
-    private Fragment homepagefragment=HomePageFragment.newInstance();
-    private Fragment myadsfragment=MyAdsFragment.newInstance();
-    private Fragment myscreenfragment=MyScreenFragment.newInstance();
-    private Fragment reportformsfragment=ReportFormsFragment.newInstance();
-    private Fragment minefragment=MineFragment.newInstance();
+    @BindView(R.id.mImageHomePage)
+    ImageView mImageHomePage;
+    @BindView(R.id.mTextHomePage)
+    TextView mTextHomePage;
+    @BindView(R.id.mImageMyAds)
+    ImageView mImageMyAds;
+    @BindView(R.id.mTextMyAds)
+    TextView mTextMyAds;
+    @BindView(R.id.mImageMyScreen)
+    ImageView mImageMyScreen;
+    @BindView(R.id.mTextMyScreen)
+    TextView mTextMyScreen;
+    @BindView(R.id.mImageReportForms)
+    ImageView mImageReportForms;
+    @BindView(R.id.mTextReportForms)
+    TextView mTextReportForms;
+    @BindView(R.id.mImageMine)
+    ImageView mImageMine;
+    @BindView(R.id.mTextMine)
+    TextView mTextMine;
+    private Fragment homepagefragment = HomePageFragment.newInstance();
+    private Fragment myadsfragment = MyAdsFragment.newInstance();
+    private Fragment myscreenfragment = MyScreenFragment.newInstance();
+    private Fragment reportformsfragment = ReportFormsFragment.newInstance();
+    private Fragment minefragment = MineFragment.newInstance();
 
 
     @Override
@@ -78,20 +101,65 @@ public class MenuActivity extends BaseActivity {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.mTabHomePage:
-                mVPFmain.setCurrentItem(0);
+                if (mVPFmain.getCurrentItem() != 0)
+                    setStarts(0);
                 break;
             case R.id.mTabMyAds:
-                mVPFmain.setCurrentItem(1);
+                if (mVPFmain.getCurrentItem() != 1)
+                    setStarts(1);
                 break;
             case R.id.mTabMyScreen:
-                mVPFmain.setCurrentItem(2);
+                if (mVPFmain.getCurrentItem() != 2)
+                    setStarts(2);
                 break;
             case R.id.mTabReportForms:
-                mVPFmain.setCurrentItem(3);
+                if (mVPFmain.getCurrentItem() != 3)
+                    setStarts(3);
                 break;
             case R.id.mTabMine:
-                mVPFmain.setCurrentItem(4);
+                if (mVPFmain.getCurrentItem() != 4)
+                    setStarts(4);
                 break;
         }
     }
+
+
+    void setStarts(int id) {
+        mImageHomePage.setImageResource(R.mipmap.icon_home_unselected);
+        mImageMine.setImageResource(R.mipmap.icon_mine_unselected);
+        mImageMyAds.setImageResource(R.mipmap.icon_myads_unselected);
+        mImageMyScreen.setImageResource(R.mipmap.icon_myscreen_unselected);
+        mImageReportForms.setImageResource(R.mipmap.icon_reportforms_unselected);
+
+        mTextHomePage.setTextColor(ContextCompat.getColor(this, R.color.colorMenuTextUnSelected));
+        mTextMine.setTextColor(ContextCompat.getColor(this, R.color.colorMenuTextUnSelected));
+        mTextMyAds.setTextColor(ContextCompat.getColor(this, R.color.colorMenuTextUnSelected));
+        mTextMyScreen.setTextColor(ContextCompat.getColor(this, R.color.colorMenuTextUnSelected));
+        mTextReportForms.setTextColor(ContextCompat.getColor(this, R.color.colorMenuTextUnSelected));
+
+        mVPFmain.setCurrentItem(id);
+        switch (id) {
+            case 0:
+                mImageHomePage.setImageResource(R.mipmap.icon_home_selected);
+                mTextHomePage.setTextColor(ContextCompat.getColor(this, R.color.colorMenuTextSelected));
+                break;
+            case 1:
+                mImageMyAds.setImageResource(R.mipmap.icon_myads_selected);
+                mTextMyAds.setTextColor(ContextCompat.getColor(this, R.color.colorMenuTextSelected));
+                break;
+            case 2:
+                mImageMyScreen.setImageResource(R.mipmap.icon_myscreen_selected);
+                mTextMyScreen.setTextColor(ContextCompat.getColor(this, R.color.colorMenuTextSelected));
+                break;
+            case 3:
+                mImageReportForms.setImageResource(R.mipmap.icon_reportforms_selected);
+                mTextReportForms.setTextColor(ContextCompat.getColor(this, R.color.colorMenuTextSelected));
+                break;
+            case 4:
+                mImageMine.setImageResource(R.mipmap.icon_mine_selected);
+                mTextMine.setTextColor(ContextCompat.getColor(this, R.color.colorMenuTextSelected));
+                break;
+        }
+    }
+
 }
