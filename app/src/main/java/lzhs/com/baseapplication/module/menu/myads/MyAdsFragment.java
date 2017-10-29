8d90e5dcbc64dc7ea697786d16e4555eb065863d
LZHS <1050629507@qq.com>
2017-10-29 14:42:30 +0800

    LZHS  首页底部布局

--- app/src/main/java/lzhs/com/baseapplication/module/menu/myads/MyAdsFragment.java
+++ app/src/main/java/lzhs/com/baseapplication/module/menu/myads/MyAdsFragment.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import lzhs.com.baseapplication.R;
-import lzhs.com.baseapplication.module.menu.mine.MineFragment;
 
 /**
  * 我的广告
@@ -30,5 +29,104 @@ public class MyAdsFragment extends Fragment {
     public static MyAdsFragment newInstance() {
         return new MyAdsFragment();
     }
+
+
+//    private void initEvents() {
+//
+//        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+//            @Override
+//            public void onTabSelected(TabLayout.Tab tab) {
+//                if (tab == mTabLayout.getTabAt(0)) {
+//                    one.setIcon(ContextCompat.getDrawable(MenuActivity.this, R.mipmap.ic_launcher_round));
+//                    mViewPager.setCurrentItem(0);
+//                } else if (tab == mTabLayout.getTabAt(1)) {
+//                    two.setIcon(ContextCompat.getDrawable(MenuActivity.this, R.mipmap.ic_launcher_round));
+//                    mViewPager.setCurrentItem(1);
+//                } else if (tab == mTabLayout.getTabAt(2)) {
+//                    three.setIcon(ContextCompat.getDrawable(MenuActivity.this, R.mipmap.ic_launcher_round));
+//                    mViewPager.setCurrentItem(2);
+//                } else if (tab == mTabLayout.getTabAt(3)) {
+//                    four.setIcon(ContextCompat.getDrawable(MenuActivity.this, R.mipmap.ic_launcher_round));
+//                    mViewPager.setCurrentItem(3);
+//                } else if (tab == mTabLayout.getTabAt(4)) {
+//                    five.setIcon(ContextCompat.getDrawable(MenuActivity.this, R.mipmap.ic_launcher_round));
+//                    mViewPager.setCurrentItem(4);
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onTabUnselected(TabLayout.Tab tab) {
+//                if (tab == mTabLayout.getTabAt(0))
+//                    one.setIcon(ContextCompat.getDrawable(MenuActivity.this, R.mipmap.ic_launcher));
+//                else if (tab == mTabLayout.getTabAt(1))
+//                    two.setIcon(ContextCompat.getDrawable(MenuActivity.this, R.mipmap.ic_launcher));
+//                else if (tab == mTabLayout.getTabAt(2))
+//                    three.setIcon(ContextCompat.getDrawable(MenuActivity.this, R.mipmap.ic_launcher));
+//                else if (tab == mTabLayout.getTabAt(3))
+//                    four.setIcon(ContextCompat.getDrawable(MenuActivity.this, R.mipmap.ic_launcher));
+//                else if (tab == mTabLayout.getTabAt(4))
+//                    five.setIcon(ContextCompat.getDrawable(MenuActivity.this, R.mipmap.ic_launcher));
+//            }
+//
+//            @Override
+//            public void onTabReselected(TabLayout.Tab tab) {
+//
+//            }
+//        });
+//
+//    }
+//
+//    private void initViews() {
+//        mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+//
+//            private String[] mTitles = new String[]{"首页", "我的广告", "我的屏幕", "报表", "我"};
+//
+//            @Override
+//            public BaseFragment_V4 getItem(int position) {
+//
+//                switch (position) {
+//                    case 0:
+//                        return OneFragment.newInstance(mTitles[position]);
+//                    case 1:
+//                        return TwoFragment.newInstance(mTitles[position]);
+//                    case 2:
+//                        return ThreeFragment.newInstance(mTitles[position]);
+//                    case 3:
+//                        return FourFragment.newInstance(mTitles[position]);
+//                    case 4:
+//                        return new FiveFragment();
+//                }
+//
+//                return null;
+//            }
+//
+//            @Override
+//            public int getCount() {
+//                return mTitles.length;
+//            }
+//
+//            @Override
+//            public CharSequence getPageTitle(int position) {
+//                return mTitles[position];
+//            }
+//
+//        });
+//
+//        mTabLayout.setupWithViewPager(mViewPager);
+//
+//        one = mTabLayout.getTabAt(0);
+//        two = mTabLayout.getTabAt(1);
+//        three = mTabLayout.getTabAt(2);
+//        four = mTabLayout.getTabAt(3);
+//        five = mTabLayout.getTabAt(4);
+//
+//        one.setIcon(ContextCompat.getDrawable(this, R.mipmap.ic_launcher));
+//        two.setIcon(ContextCompat.getDrawable(this, R.mipmap.ic_launcher));
+//        three.setIcon(ContextCompat.getDrawable(this, R.mipmap.ic_launcher));
+//        four.setIcon(ContextCompat.getDrawable(this, R.mipmap.ic_launcher));
+//        five.setIcon(ContextCompat.getDrawable(this, R.mipmap.ic_launcher));
+//    }
+
 
 }
