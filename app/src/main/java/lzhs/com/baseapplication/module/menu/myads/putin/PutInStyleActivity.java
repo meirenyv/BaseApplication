e5a4e0c09f575c5274199311bfd5c3fbec4a8b6d
meirenyv <2366081200@qq.com>
2017-11-06 09:32:28 +0800

    充值，购物车

--- app/src/main/java/lzhs/com/baseapplication/module/menu/myads/putin/PutInStyleActivity.java
+++ app/src/main/java/lzhs/com/baseapplication/module/menu/myads/putin/PutInStyleActivity.java
@@ -1,18 +1,21 @@
 package lzhs.com.baseapplication.module.menu.myads.putin;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.Nullable;
 
 import lzhs.com.baseapplication.R;
+import lzhs.com.library.base.BaseActivity;
+import lzhs.com.library.utils.ActivityUtil;
 
 /**
  * 投放广告类型选择： 普通，自投 ，插播
  */
-public class PutInStyleActivity extends AppCompatActivity {
+public class PutInStyleActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_put_in_style);
+        ActivityUtil.setStatusBarBackground(this);
     }
 }
